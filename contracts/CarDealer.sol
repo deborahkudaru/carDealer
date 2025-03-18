41783640d3ffd265070b380da81672d11092d49c
Kudaru Deborah Nshuk <deborahkudaru@gmail.com>
2025-03-18 19:55:05 +0100

    update

--- contracts/CarDealer.sol
+++ contracts/CarDealer.sol
@@ -24,6 +24,8 @@ contract CarDealer is ERC721URIStorage {
     uint256 private nextCarID;
     mapping(uint256 => Car) public cars;
     mapping(uint256 => address[]) public carHistory;
+    mapping(address => uint256[]) private userCars;
+    uint256[] private listedCars;
 
     constructor() ERC721("CarNFT", "GLK") {}
 
@@ -45,6 +47,9 @@ contract CarDealer is ERC721URIStorage {
             previousOwners: new address[](0)
         });
 
+        userCars[msg.sender].push(carId);
+        listedCars.push(carId);
+
         _mint(msg.sender, carId);
         _setTokenURI(carId, _tokenURI);
         emit Events.CarRegistered(carId, msg.sender);
@@ -60,6 +65,8 @@ contract CarDealer is ERC721URIStorage {
 
         cars[_carId].forSale = true;
         cars[_carId].price = _price;
+
+        listedCars.push(_carId);
 
         emit Events.CarListed(_carId, _price);
     }
@@ -85,7 +92,33 @@ contract CarDealer is ERC721URIStorage {
 
         car.forSale = false;
 
+        for (uint256 i = 0; i < listedCars.length; i++) {
+            if (listedCars[i] == _carId) {
+                listedCars[i] = listedCars[listedCars.length - 1];
+                listedCars.pop();
+                break;
+            }
+        }
+
+        userCars[msg.sender].push(_carId);
         emit Events.CarSold(_carId, msg.sender, msg.value);
+    }
+
+    function getCarsForSale() external view returns (Car[] memory) {
+        Car[] memory saleCars = new Car[](listedCars.length);
+        for (uint256 i = 0; i < listedCars.length; i++) {
+            saleCars[i] = cars[listedCars[i]];
+        }
+        return saleCars;
+    }
+
+    function getMyCars() external view returns (Car[] memory) {
+        uint256[] memory myCarIds = userCars[msg.sender];
+        Car[] memory myCars = new Car[](myCarIds.length);
+        for (uint256 i = 0; i < myCarIds.length; i++) {
+            myCars[i] = cars[myCarIds[i]];
+        }
+        return myCars;
     }
 
     function getCarHistory(
